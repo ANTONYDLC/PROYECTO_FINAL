af3e1351703d86e84ee4f689a1c0b9b13ee09211
ANTONYDLC <antonydelacruz12082002@gmail.com>
2025-06-12 18:46:45 -0500

    dockerfile

--- HamburguesasTienda/obj/Debug/net8.0/HamburguesasTienda.AssemblyInfo.cs
+++ HamburguesasTienda/obj/Debug/net8.0/HamburguesasTienda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HamburguesasTienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed61c7fc1387bb6150e938bd93c69efe5269efa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3235b4b3824b436d618dbdb0fb245e2d647fbd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HamburguesasTienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HamburguesasTienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
